1d6dd8bebeae09e913b2b1a370c73a6c40b8a754
Cappelletti, Jaden Mariano <jamcapp@iu.edu>
2022-04-13 23:52:34 -0400

    Working on CUD for user
    Working on create, update, and delete for users

--- routes/users.js
+++ routes/users.js
@@ -17,6 +17,48 @@ router.get("/:id", getUser, (req, res) => {
   res.json(res.user);
 });
 
+//Creating one user
+router.post("/", async (req, res) => {
+  const user = new User({
+    _id: req.body._id,
+    name: req.body.name,
+    password: req.body.stats,
+  });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+//Updating one user
+router.patch("/:id", getUser, async (req, res) => {
+  if (req.body.name != null) {
+    res.item.name = req.body.name;
+  }
+  if (req.body.password != null) {
+    res.item.password = req.body.password;
+  }
+  try {
+    const updatedUser = await res.user.save();
+    res.json(updatedUser);
+  } catch (err) {
+    res.stats(400).json({ message: err.message });
+  }
+});
+
+//Deleting one user
+router.delete("/:id", getUser, async (req, res) => {
+  try {
+    await res.user.remove();
+    res.json({ message: "Deleted user" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 async function getUser(req, res, next) {
   let user;
   try {
