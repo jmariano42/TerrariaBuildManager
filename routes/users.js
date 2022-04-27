de37ce9c0c7cd7bb8ee3fcb77ffd0be4c92ed610
Cappelletti, Jaden Mariano <jamcapp@iu.edu>
2022-04-26 23:10:47 -0400

    Can now save new builds
    User can now save new builds

--- routes/users.js
+++ routes/users.js
@@ -16,6 +16,20 @@ router.all("/", function (req, res, next) {
   next();
 });
 
+router.all("/:id", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "PATCH, POST, PUT, DELETE, GET, OPTIONS"
+  );
+  res.header("Access-Control-Request-Method", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 //Getting all users
 router.get("/", async (req, res) => {
   try {
